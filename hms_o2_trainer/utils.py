3bbac0598b3ce0479e69a811153cf76e712c7395
Kale Kundert <kale@thekunderts.net>
2024-05-27 13:54:52 -0400

    feat: distinguish between batch/interactive jobs

--- hms_o2_trainer/utils.py
+++ hms_o2_trainer/utils.py
@@ -1,4 +1,5 @@
 import os
+import re
 import logging
 
 log = logging.getLogger('hms_o2_trainer')
@@ -6,6 +7,20 @@ info = log.info
 
 def is_slurm():
     return 'SLURM_JOB_ID' in os.environ
+
+def is_sbatch():
+    from subprocess import run
+
+    try:
+        job_id = os.environ['SLURM_JOB_ID']
+    except KeyError:
+        return False
+
+    squeue = 'squeue', '--Format=batchflag', '--jobs', job_id
+    p = run(squeue, capture_output=True)
+    m = re.fullmatch(r'BATCH_FLAG\s*\n(\d)\s*\n', p.stdout.decode())
+    
+    return m and m.group(1) == '1'
 
 def get_job_id():
     array_job_id = os.getenv('SLURM_ARRAY_JOB_ID')
