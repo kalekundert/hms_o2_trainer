b82f77c21c01ebfde64e95a26c4f73d10057b7ad
Kale Kundert <kale@thekunderts.net>
2025-01-11 17:31:28 -0500

    feat: add utility to get available VRAM

--- hms_o2_trainer/utils.py
+++ hms_o2_trainer/utils.py
@@ -29,6 +29,10 @@ def is_sbatch():
 def if_gpu(gpu_value, cpu_value):
     return gpu_value if torch.cuda.is_available() else cpu_value
 
+def get_vram_gb():
+    vram_free, vram_tot = torch.cuda.mem_get_info()
+    return vram_tot / 1e9
+
 def get_job_id():
     # Note that for array jobs, `squeue` displays job ids in the format `<id of 
     # first job in array>_<task id>`.  These aren't really job ids, though, so 
